f49332d60377e95b26c871aa326e47421d4f6797
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2025-04-19 21:28:42 +0300

    Add style in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/base/globals.css';
-//import '@/styles/layout/header.css';
+import '@/styles/base/variables.css';
+import '@/styles/layout/header.module.css';
 //import '@/styles/layout/footer.css';
 //import '@/styles/layout/intro.css';
 import '@/styles/responsive/mobile.css';
