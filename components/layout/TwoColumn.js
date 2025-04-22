dc1dfc3b6b10dc88474176e8718a1f4663a3e703
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2025-04-22 09:49:34 +0300

    Fix two column page

--- components/layout/TwoColumn.js
+++ components/layout/TwoColumn.js
@@ -2,9 +2,10 @@ import styles from '@/styles/layout/two-column.module.css';
 
 export default function TwoColumn({ left, right }) {
   return (
-    <div className={styles.twoColumnWrapper}>
-      <div className={styles.column}>{left}</div>
-      <div className={styles.column}>{right}</div>
+    <div className={styles.twoColumn}>
+      <div className={styles.left}>{left}</div>
+      <div className={styles.right}>{right}</div>
     </div>
   );
 }
+
